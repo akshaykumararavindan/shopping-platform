5c54a62517f84533ef50c778d36c71711fef49e5
Akshay Kumar Aravindan <balakshay@live.com>
2020-12-05 12:50:36 +1100

    Project Files

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,40 @@
+import Order from "../models/orderModel.js";
+import asyncHandler from "express-async-handler";
+
+//@desc     Create new order
+//@route    GET /api/orders
+//@access   Private route
+const addOrderItems = asyncHandler(async (req, res) => {
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    ShippingPrice,
+    totalPrice,
+  } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error("No order Items");
+    return;
+  } else {
+    const order = new Order({
+      orderItems,
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      ShippingPrice,
+      totalPrice,
+    });
+
+    const createdOrder = await order.save();
+
+    res.status(201).json(createdOrder);
+  }
+});
+
+export { addOrderItems };
---
+++ backend/models/orderModel.js
@@ -0,0 +1,74 @@
+import mongoose from 'mongoose';
+
+const orderSchema = mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    orderItems: [{
+        name: { type: String, requried: true },
+        qty: { type: Number, requried: true },
+        image: { type: String, requried: true },
+        price: { type: Number, requried: true },
+        product: { 
+            type: mongoose.Schema.Types.ObjectId, 
+            requried: true, 
+            ref: 'Product' 
+        },
+    }],
+    shippingAddress: {
+        address: { type: String, required: true },
+        city: { type: String, required: true },
+        postalCode: { type: String, required: true },
+        country: { type: String, required: true },
+    },
+    paymentMethod: {
+        type: String,
+        required: true,
+    },
+    paymentResult: {
+        id: { type: String },
+        status: { type: String },
+        update_time: { type: String },
+        email_address: { type: String },
+    },
+    taxPrice: {
+        type: String,
+        required: true,
+        default: 0.0
+    },
+    shippingPrice: {
+        type: String,
+        required: true,
+        default: 0.0
+    },
+    totalPrice: {
+        type: String,
+        required: true,
+        default: 0.0
+    },
+    isPaid: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    paidAt: {
+        type: Date,
+    },
+    isDelivered: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    deliveredAt: {
+        type: Date,
+    },
+}, 
+{
+    timestamps: true
+});
+
+const Order = mongoose.model('Order', orderSchema);
+
+export default Order;
