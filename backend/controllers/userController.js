2661b28d0db8bff43b1ef8299dd01a8a04a3363e
Akshay Kumar <balakshay@live.com>
2020-12-16 12:38:14 +1100

    More Changes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -76,7 +76,6 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-
 //@desc     Update user profile
 //@route    PUT /api/users/profile
 //@access   Private
@@ -84,10 +83,10 @@ const updateUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
 
   if (user) {
-    user.name = req.body.name || user.name
-    user.email = req.body.email || user.email
-    if(req.body.password) {
-      user.password = req.body.password
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
     }
     const updatedUser = await user.save();
 
@@ -98,11 +97,18 @@ const updateUserProfile = asyncHandler(async (req, res) => {
       isAdmin: updatedUser.isAdmin,
       token: generateToken(updatedUser._id),
     });
-
   } else {
     res.status(404);
     throw new Error("User not found");
   }
 });
 
-export { authUser, getUserProfile, registerUser, updateUserProfile };
+//@desc     Get All Users
+//@route    GET /api/users/profile
+//@access   Private/Admin
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+});
+
+export { authUser, getUserProfile, registerUser, updateUserProfile, getUsers };
