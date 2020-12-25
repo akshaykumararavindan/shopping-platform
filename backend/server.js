d2ef1f577a77d6c1eb8f7a6db6d68bd2e9609d3b
Akshay Kumar <balakshay@live.com>
2020-12-26 10:28:50 +1100

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -34,16 +34,24 @@ app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
 
-const folder = path.resolve();
-app.use("/uploads", express.static(path.join(folder, "/uploads")));
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (_, res) => {
+    // _ is just optional parameter. Not being used here!
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (_, res) => {
+    res.send("API is running...");
+  });
+}
 
 app.use(notFound);
 
 app.use(errorHandler);
-
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
 
 app.listen(
   PORT,
